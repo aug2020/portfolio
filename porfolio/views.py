86b831bd6ab6b468f556dc8040150a9461e727c0
Augustine Abimbola <augustineabimbola@Augustines-MacBook-Pro.local>
2020-08-17 12:33:36 -0400

    first commit

---
+++ porfolio/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from .models import Project
+# Create your views here.
+def homepage(request):
+    projects = Project.objects.all() # this grabs all the project objects
+    return render(request,'porfolio/projects.html',{'projects': projects})
