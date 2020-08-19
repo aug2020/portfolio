dccd7265c41fa4b7cc3b8ef4e55e0fa3f750559b
Augustine Abimbola <augustineabimbola@Augustines-MacBook-Pro.local>
2020-08-19 08:14:59 -0400

    Added links to blogs and added new url blog paths

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render ,get_object_or_404
 from .models import Blog
 # Create your views here.
 def all_blogs(request):
 
     blog = Blog.objects.all()
-
     return render(request,'blog/all_blogs.html',{'blogs':blog})
+def detail(request,blog_id):
+    #here were going to grab the blog based on the blog id using the get_object_or_404 module
+
+    blog = get_object_or_404(Blog,pk=blog_id)
+
+    #pk is short for primary key
+    #so pk is the id of the blog post in the model, so if there is a primary key that relates to the blogid given to the user then it will show the blog else it will show a 404 page
+
+    return render(request,'blog/detail.html',{'blog':blog})
